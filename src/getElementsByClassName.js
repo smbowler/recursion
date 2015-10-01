7ba23b6367e8ca4ec03c790d682e8827611156b8
Samuel McNamara Bowler <SamuelMcNamaraBowler@Samuels-MacBook-Pro.local>
2015-10-01 17:19:55 -0500

    Modified getElementsByClassName to get tests passing

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,23 +4,32 @@
 // };
 
 // But instead we're going to implement it from scratch:
+// var getElementsByClassName = function(className){
+//   var matchingElements = [];
+//   var findMatchingElements = function(element){
+//     if (element.classList.contains(className)){
+//       matchingElements.push(element);
+//     }
+//     for(var i = 0; i < element.children.length; i++) {
+//       findMatchingElements(element.children[i]);
+//     }
+//   }
+//   findMatchingElements(document.body);
+//   return matchingElements;
+// };
+
+
 var getElementsByClassName = function(className){
-    var result = [];
-
-  // Starting node
-  Node = Node || document.body;
-
-  // check for className of starting node // add to results if available 
-  if(Node.className === "targetClassName"){
-    return true;
-  }
-
-  // Check node children for className
-  if(Node.children){
-    for(var i = 0, count = Node.children.length; i < count; i++){
-      // recursively call on children and check for className
-      result = result.concat(getElementsByClassName(className, Node.children[i]) );
+  var findMatchingElements = function(element){
+    var allMatchingElements = [];
+    if (element.classList.contains(className)){
+      allMatchingElements.push(element);
     }
+    for(var i = 0; i < element.children.length; i++) {
+      var childMatchingElements = findMatchingElements(element.children[i]);
+      allMatchingElements = allMatchingElements.concat(childMatchingElements);
+    }
+    return allMatchingElements;
   }
-  return result;
+  return findMatchingElements(document.body);
 };
